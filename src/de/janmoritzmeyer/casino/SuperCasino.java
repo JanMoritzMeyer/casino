509244d56498ccef9e594ee27789c1c20ae0a30a
JanMoritz <jan@janmoritzmeyer.de>
2019-06-03 12:30:08 +0200

    Kommentare für SuperCasino

--- src/de/janmoritzmeyer/casino/SuperCasino.java
+++ src/de/janmoritzmeyer/casino/SuperCasino.java
@@ -14,6 +14,7 @@ public abstract class SuperCasino extends Casino {
 
     /* Class for providing Elements and functions for the Casino */
 
+    //Funktion zum ausgeben der Bilder als Icon mit der richtigen Größe für die Komponenten
     public ImageIcon scaleImage(String name, int x, int y){
         ImageIcon imageIcon = new ImageIcon("Images/"+name); // Bild laden
         Image image = imageIcon.getImage(); // Icon zu Bild umwandeln
@@ -22,11 +23,15 @@ public abstract class SuperCasino extends Casino {
         return imageIcon; //Icon zurückgeben
     }
 
+    //Vereinfachte Funktion um eine zufällige Zahl zu erhalten
     public int random(int min, int max){
         Random r = new Random();
+        //Grenzen für die Zufallszahl festlegen
         return r.nextInt((max - min) + 1) + min;
     }
 
+    //Funktion, welche die Buttons vereinfacht erstellt, welche für das Roulette gebraucht werden
+    //Das Setzen erklärt sich von selber
     protected JButton robutton(String text, int xpos, int ypos, int width, int height, ActionListener actionListener){
         JButton button = new JButton( text );
         button.setSize( width, height );
@@ -39,20 +44,27 @@ public abstract class SuperCasino extends Casino {
         return button;
     }
 
+    //vereinfachte Funktion zum abspielen von audio Dateien
     public void audioPlayer(String file, boolean loop){
+        //Lokale Variablen deklarieren
         AudioInputStream audioInputStream;
         Clip clip;
         try {
+            //initialisieren der AudioFiles
             audioInputStream = AudioSystem.getAudioInputStream(new File("Sounds/"+file).getAbsoluteFile());
             clip = getClip();
+            //Clip öffnen
             clip.open(audioInputStream);
+            //Wenn Sound geloopt wird, Loop einstellen, sonst Sound starten
             if(loop){
                 clip.loop( Clip.LOOP_CONTINUOUSLY);
             }
             else {
                 clip.start();
             }
-        } catch (UnsupportedAudioFileException e) {
+        }
+        //catch Funktionen falls Fehler auftreten
+        catch (UnsupportedAudioFileException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
